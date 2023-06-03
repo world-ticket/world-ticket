5f148bdd13d6750256cc9f383e230f5254e5d73a
somasekimoto <0421.soma@gmail.com>
2023-06-03 18:03:05 +0900

    WIP: Ticket NFT

---
+++ packages/contract/contracts/Ticket.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import { ByteHasher } from "./helpers/ByteHasher.sol";
+import { IWorldID } from "./interfaces/IWorldID.sol";
+
+contract WorldTicket is ERC721 {
+    using Counters for Counters.Counter;
+    using ByteHasher for bytes;
+
+    /// @notice Thrown when attempting to reuse a nullifier
+    error InvalidNullifier();
+
+    Counters.Counter public tokenId;
+
+    /// @dev The World ID instance that will be used for verifying proofs
+    IWorldID internal immutable worldId;
+
+    /// @dev The contract's external nullifier hash
+    uint256 internal immutable externalNullifier;
+
+    /// @dev The World ID group ID (always 1)
+    uint256 internal immutable groupId = 1;
+
+    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
+    mapping(uint256 => bool) internal nullifierHashes;
+
+    event Mint(address indexed _to, uint256 indexed _tokenId);
+
+    constructor(IWorldID _worldId, string memory _appId, string memory _actionId) ERC721("WorldTicket", "WTC") {
+        worldId = _worldId;
+        externalNullifier = abi
+            .encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
+            .hashToField();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://worldticket.io/api/ticket/";
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        return tokenURI(_tokenId);
+    }
+
+    function verify(
+        address signal,
+        uint256 root,
+        uint256 nullifierHash,
+        uint256[8] calldata proof
+    ) private {
+        // First, we make sure this person hasn't done this before
+        if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
+
+        // We now verify the provided proof is valid and the user is verified by World ID
+        worldId.verifyProof(
+            root,
+            groupId,
+            abi.encodePacked(signal).hashToField(),
+            nullifierHash,
+            externalNullifier,
+            proof
+        );
+
+        // We now record the user has done this, so they can't do it again (proof of uniqueness)
+        nullifierHashes[nullifierHash] = true;
+
+        // Finally, execute your logic here, for example issue a token, NFT, etc...
+        // Make sure to emit some kind of event afterwards!
+    }
+        
+
+    function mint(
+        address _signal,
+        uint256 _root,
+        uint256 _nullifierHash,
+        uint256[8] calldata _proof,
+        address _to
+    ) public {
+        verify(
+            _signal,
+            _root,
+            _nullifierHash,
+            _proof
+        );
+        uint256 _tokenId = tokenId.current();
+        _safeMint(_to, _tokenId);
+        tokenId.increment();
+        emit Mint(_to, _tokenId);
+    }
+
+    function buy(
+        address _signal,
+        uint256 _root,
+        uint256 _nullifierHash,
+        uint256[8] calldata _proof,
+        address _to
+    ) public payable {
+        worldId.verifyProof(
+            _root,
+            groupId,
+            abi.encodePacked(_signal).hashToField(),
+            _nullifierHash,
+            externalNullifier,
+            _proof
+        );
+        revert("verification success");
+    }
+
+    function sell(
+        address _signal,
+        uint256 _root,
+        uint256 _nullifierHash,
+        uint256[8] calldata _proof,
+        address _to
+    ) public payable {
+        worldId.verifyProof(
+            _root,
+            groupId,
+            abi.encodePacked(_signal).hashToField(),
+            _nullifierHash,
+            externalNullifier,
+            _proof
+        );
+        revert("verification success");
+    }
+
+
+}
